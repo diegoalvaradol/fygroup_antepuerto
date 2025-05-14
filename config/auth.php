f19ae6e41a4476c8f834c755e9c61c68ecdf869e
Diego Alvarado Lopez <diego.alvaraado@gmail.com>
2025-05-14 12:17:41 -0400

    [Mejora][SSL] cambios v2

--- config/auth.php
+++ config/auth.php
@@ -1,10 +1,11 @@
 <?php
 session_start();
-$exemptFiles = ['login.php', 'register.php'];
 
 $currentFile = basename($_SERVER['PHP_SELF']);
+$exemptFiles = ['login.php', 'register.php'];
+
 if (!isset($_SESSION['user']) && !in_array($currentFile, $exemptFiles)) {
   $redirectBack = urlencode($_SERVER['REQUEST_URI']);
-  header("Location: login.php?redirect=$redirectBack");
+  header("Location: /ssl-chile/mySSL/login.php?redirect=$redirectBack");
   exit();
 }
