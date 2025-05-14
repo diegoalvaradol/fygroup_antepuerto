13a14727b279091350be5629bfa2300cb6916687
Diego Alvarado Lopez <diego.alvaraado@gmail.com>
2025-05-14 16:18:46 -0400

    [Mejora][SSL] mejoras varias v4

--- mySSL/logout.php
+++ mySSL/logout.php
@@ -3,5 +3,5 @@ session_start();
 session_unset();
 session_destroy();
 
-header("Location: ../mySSL/login.php");
+header("Location: login.php");
 exit();
--- portal/logout.php
+++ portal/logout.php
@@ -3,7 +3,5 @@ session_start();
 session_unset();
 session_destroy();
 
-//header("Location: ../portal/login.php");
-//exit();
 header("Location: login.php");
 exit();
