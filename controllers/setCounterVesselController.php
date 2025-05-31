6ffe1901954dedd47cf3b269eafd87c19f1679be
Diego Alvarado Lopez <diego.alvaraado@gmail.com>
2025-05-30 21:55:21 -0400

    [Nuevo][SSL] contador de camiones por nave

---
+++ controllers/setCounterVesselController.php
@@ -0,0 +1,21 @@
+<?php
+require_once __DIR__ . '/../config/database.php';
+
+if (isset($_POST['id'], $_POST['origin'])) {
+  $db = (new Database())->getConnection();
+
+  $id     = $_POST['id'];
+  $origin = $_POST['origin'];
+
+  $query = "SELECT * FROM app_outer_port WHERE vessel_id = :id AND origin = :origin ORDER BY row_id DESC LIMIT 1";
+  $stmt  = $db->prepare($query);
+  $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+  $stmt->bindParam(":origin", $origin, PDO::PARAM_INT);
+  $stmt->execute();
+  $result = $stmt->fetch(PDO::FETCH_ASSOC);
+  $count  = $result['counter_vessel'] ?? 0;
+
+  $counter = htmlspecialchars($count + 1);
+
+  echo $counter;
+}
