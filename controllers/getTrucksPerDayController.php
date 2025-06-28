882409f9a93b6cadc676ecc5eb4739a5f7366983
Diego Alvarado <dalvarado@gotruck.cl>
2025-06-28 19:17:10 -0400

    [Nuevo][SSL] añade input fechas en grafico

---
+++ controllers/getTrucksPerDayController.php
@@ -0,0 +1,10 @@
+<?php
+require_once __DIR__ . '/../config/includes.php';
+$db = (new Database())->getConnection();
+
+$input  = json_decode(file_get_contents("php://input"), true);
+$inicio = $input['fechaInicio'];
+$fin    = $input['fechaFin'];
+
+$port = new outerPort($db); // usa tu clase real si tiene otro nombre
+echo $port->trucksPerDay($inicio, $fin);
