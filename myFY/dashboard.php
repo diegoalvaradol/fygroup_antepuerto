9283090bb496f32307a3c8b731e4ffbb19db143c
Diego Alvarado López <diego.alvaraado@gmail.com>
2026-04-09 12:44:24 -0400

    [Mejora][FYGroup] downgrade bootstrap 5.3 a 4.6 v2

--- myFY/dashboard.php
+++ myFY/dashboard.php
@@ -456,7 +456,6 @@ $whatsAppBtn = UIComponents::whatsappChatBox();
     <!-- Select2 JS -->
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
-
     <!-- Chat flotante expandible estilo WhatsApp - Responsive -->
     <?php echo $whatsAppBtn; ?>
 </body>
