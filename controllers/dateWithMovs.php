efeeb10dd35eeab25dbaedb32c12f9147182cd8d
Diego Alvarado López <diego.alvaraado@gmail.com>
2026-05-11 17:15:52 -0400

    [Mejora][FYGroup] mejora en controller datewithmovs v1

--- controllers/dateWithMovs.php
+++ controllers/dateWithMovs.php
@@ -1,17 +1,18 @@
 <?php
+
+declare(strict_types=1);
 require_once __DIR__ . '/../config/includes.php';
 
 $outerPort = new outerPort();
+$fechas = [];
 
-$sql  = "SELECT DISTINCT DATE(arrival_date) AS fecha FROM app_outer_port GROUP BY DATE(arrival_date) ORDER BY fecha DESC";
+$sql = 'SELECT DISTINCT DATE(arrival_date) AS fecha FROM app_outer_port GROUP BY DATE(arrival_date) ORDER BY fecha DESC';
 $list = $outerPort->findAllStatic($sql);
 
 if ($list->length()) {
-  $fechas = [];
-
-  foreach ($list->getCollection() as $row) {
-    $fechas[] = $row['fecha'];
-  }
+    foreach ($list->getCollection() as $row) {
+        $fechas[] = $row['fecha'];
+    }
 }
 
 header('Content-Type: application/json');
