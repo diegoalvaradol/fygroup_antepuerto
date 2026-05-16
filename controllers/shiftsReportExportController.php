6c9bbb2a8ba0a4fb6aad362fcf61a10e507c916f
Diego Alvarado López <diego.alvaraado@gmail.com>
2026-05-16 15:32:09 -0400

    [Mejora][FYGroup] elimina muestra de errores

--- controllers/shiftsReportExportController.php
+++ controllers/shiftsReportExportController.php
@@ -3,9 +3,6 @@
 declare(strict_types=1);
 require_once __DIR__ . '/../config/includes.php';
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
 $outerPort = new outerPort();
 
 $date = $_GET['date'] ?? null;
