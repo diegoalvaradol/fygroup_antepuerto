a5ec48d4c27ef12d080493ff3ae3375e08b23876
Diego Alvarado López <diego.alvaraado@gmail.com>
2026-05-24 13:09:53 -0400

    [Mejora][FYGroup] mejoras full dejs en paginas v1

--- myFY/program_msc.php
+++ myFY/program_msc.php
@@ -125,10 +125,6 @@ if (!$admin) {
     <!-- Custom scripts for all pages-->
     <script src="../assets/js/fygroup.js"></script>
 
-    <!-- Bootstrap JS (necesario para popover) -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-
     <!-- Select2 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet"/>
 
