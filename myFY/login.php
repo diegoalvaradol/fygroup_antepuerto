ab033320c8f1d9c5d07e703aff64d3390a2a777e
Diego Alvarado López <diego.alvaraado@gmail.com>
2026-05-30 12:09:11 -0400

    [Mejora][FYGroup] mejoras full js y vistas v2

--- myFY/login.php
+++ myFY/login.php
@@ -4,22 +4,30 @@ if (isset($_SESSION['user'])) {
 
     exit();
 }
+
+require_once __DIR__ . '/../config/includes.php';
+
+$footer = menu::footerSSL();
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
 
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>FYGroup | Login</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>FYGroup | Login</title>
 
-  <link rel="icon" type="image/png" href="../favicon/apple-touch-icon.png"/>
-  <link href="../assets/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,300,400,600,700,800,900" rel="stylesheet">
-  <link href="../assets/css/fygroup.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="icon" type="image/png" href="../favicon/apple-touch-icon.png"/>
+    <link href="../assets/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,300,400,600,700,800,900" rel="stylesheet">
+    <link href="../assets/css/fygroup.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <!-- Custom styles FYGroup-->
+    <link rel="stylesheet" href="../assets/css/footer.css">
+    <script src="../assets/js/sidebar.js"></script>
 </head>
 
 <style>
@@ -77,50 +85,66 @@ if (isset($_SESSION['user'])) {
 </style>
 
 <body>
-  <div class="container login-wrapper d-flex justify-content-center align-items-center min-vh-100">
-    <div class="col-xl-4 col-lg-5 col-md-7">
-      <div class="card login-card p-4">
-        <div class="text-center mb-4">
-          <img src="../images/logo-fygroup-circle-v1.png" class="logo-img mb-3">
-          <h4 class="font-weight-bold text-dark mb-1">Sistema Integral FYGroup</h4>
-          <small class="text-muted">Acceso Personal</small>
+    <div class="container login-wrapper d-flex justify-content-center align-items-center min-vh-100">
+        <div class="col-xl-4 col-lg-5 col-md-7">
+            <div class="card login-card p-4">
+                <div class="text-center mb-4">
+                    <img src="../images/logo-fygroup-circle-v1.png" class="logo-img mb-3">
+                    <h4 class="font-weight-bold text-dark mb-1">Sistema Integral FYGroup</h4>
+                    <small class="text-muted">Acceso Personal</small>
+                </div>
+
+                <form id="loginForm">
+                    <div class="form-group mb-3">
+                        <label class="small text-muted">R.U.N</label>
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-user"></i></span>
+                            </div>
+                            <input type="text" class="form-control text-center" id="run" name="run" autocomplete="run" maxlength="12" placeholder="12.345.678-9" oninput="formatearRun(this)" onblur="validaRun(this.value)">
+                        </div>
+                    </div>
+
+                    <div class="form-group mb-4">
+                        <label class="small text-muted">Contraseña</label>
+                            <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-lock"></i></span>
+                            </div>
+                            <input type="password" class="form-control text-center" id="password" name="password" autocomplete="current-password" placeholder="••••••••">
+                        </div>
+                    </div>
+
+                    <button id="loadBtn" type="button" onclick="loadSession()"class="btn btn-primary btn-login btn-block">
+                        <span id="loadBtnText"><i class="fas fa-right-to-bracket mr-2"></i> Iniciar Sesión</span>
+                        <span id="loadBtnSpinner" class="spinner-border spinner-border-sm d-none"></span>
+                    </button>
+
+                    <div class="mt-3">
+                        <a href="https://wa.me/56923816700?text=Hola%20necesito%20ayuda" target="_blank" class="btn btn-outline-success btn-block">
+                            <i class="fab fa-whatsapp mr-2"></i>
+                            Soporte por WhatsApp
+                        </a>
+                    </div>
+
+                    <div class="text-center mt-3">
+                        <small class="text-muted">
+                            ¿Tienes problemas con tu cuenta?
+                            <a href="mailto:soporte@fygroup.cl">Escríbenos</a>
+                        </small>
+                    </div>
+                </form>
+            </div>
         </div>
 
-        <form id="loginForm">
-          <div class="form-group mb-3">
-            <label class="small text-muted">R.U.N</label>
-            <div class="input-group">
-              <div class="input-group-prepend">
-                <span class="input-group-text"><i class="fas fa-user"></i></span>
-              </div>
-              <input type="text" class="form-control text-center" id="run" name="run" autocomplete="run" maxlength="12" placeholder="12.345.678-9" oninput="formatearRun(this)" onblur="validaRun(this.value)">
-            </div>
-          </div>
-
-          <div class="form-group mb-4">
-            <label class="small text-muted">Contraseña</label>
-            <div class="input-group">
-              <div class="input-group-prepend">
-                <span class="input-group-text"><i class="fas fa-lock"></i></span>
-              </div>
-              <input type="password" class="form-control text-center" id="password" name="password" autocomplete="current-password" placeholder="••••••••">
-            </div>
-          </div>
-
-          <button id="loadBtn" type="button" onclick="loadSession()"class="btn btn-primary btn-login btn-block">
-            <span id="loadBtnText"><i class="fas fa-right-to-bracket mr-2"></i> Iniciar Sesión</span>
-            <span id="loadBtnSpinner" class="spinner-border spinner-border-sm d-none"></span>
-          </button>
-        </form>
-      </div>
+        <?php echo $footer; ?>
     </div>
-  </div>
 
-  <!-- Scripts -->
-  <script src="../assets/vendor/jquery/jquery.min.js"></script>
-  <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="../assets/vendor/jquery-easing/jquery.easing.min.js"></script>
-  <script src="../assets/js/fygroup.js"></script>
+    <!-- Scripts -->
+    <script src="../assets/vendor/jquery/jquery.min.js"></script>
+    <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="../assets/js/fygroup.js"></script>
 </body>
 </html>
 
