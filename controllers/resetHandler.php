ce050a8a2287b021c0df2dc5069af92c420f69bd
Diego Alvarado López <diego.alvaraado@gmail.com>
2026-06-08 09:33:55 -0400

    [Mejora][FYGroup] cambios en gurada y actualiza usuario v1

--- controllers/resetHandler.php
+++ controllers/resetHandler.php
@@ -1,20 +1,22 @@
 <?php
+
+declare(strict_types=1);
 require_once __DIR__ . '/../config/includes.php';
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-  $token   = $_POST["token"];
-  $pass    = $_POST["password"];
-  $confirm = $_POST["password2"];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $token = $_POST['token'];
+    $pass = $_POST['password'];
+    $confirm = $_POST['password2'];
 
-  if ($pass !== $confirm) {
-    die("Las contraseñas no coinciden.");
-  }
+    if ($pass !== $confirm) {
+        die('Las contraseñas no coinciden.');
+    }
 
-  $user = new User();
+    $user = new User();
 
-  if ($user->resetPassword($token, $pass)) {
-    echo "OK";
-  } else {
-    echo "NOOK";
-  }
+    if ($user->resetPasswordToken($token, $pass)) {
+        echo 'OK';
+    } else {
+        echo 'NOOK';
+    }
 }
