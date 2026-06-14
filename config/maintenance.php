4464a3fbb4ff6b1f7511c742a84a30fb0900c28e
Diego Alvarado López <diego.alvaraado@gmail.com>
2026-06-14 14:18:27 -0400

    [Mejora][FYGroup] manejo de pagina de mantención v2

--- config/maintenance.php
+++ config/maintenance.php
@@ -2,4 +2,5 @@
 
 declare(strict_types=1);
 
-define('MAINTENANCE_MODE', false);
+define('MAINTENANCE_MODE_FYGROUP', false);
+define('MAINTENANCE_MODE_PORTAL', false);
--- myFY/login.php
+++ myFY/login.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/../config/maintenance.php';
 
-if (MAINTENANCE_MODE) {
+if (MAINTENANCE_MODE_FYGROUP) {
     require_once __DIR__ . '/../maintenance.php';
 
     exit;
