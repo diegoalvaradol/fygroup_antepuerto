37c00e4a0ed2036bff71ac0ccd7fc1a4e3bba911
Diego Alvarado López <diego.alvaraado@gmail.com>
2026-07-12 16:15:14 -0400

    [Mejora][FYGroup] cambios en nombre de bd

--- config/database.php
+++ config/database.php
@@ -1,39 +1,39 @@
 <?php
+
+declare(strict_types=1);
 require_once __DIR__ . '/../functions/functions.php';
 
 class Database
 {
-  private static ?PDO $conn = null;
+    private static ?PDO $conn = null;
 
-  private function __construct()
-  {}
-
-  public static function get(): PDO
-  {
-    if (self::$conn === null) {
-      if (esLocalhost()) {
-        $host = "localhost";
-        $db   = "ssl_chile";
-        $user = "ssl_chile";
-        $pass = "seatrade1313";
-      } else {
-        $host = "localhost";
-        $db   = "fygroupc_myfy";
-        $user = "fygroupc_adminfy";
-        $pass = "Seatrade1313_";
-      }
-
-      self::$conn = new PDO(
-        "mysql:host={$host};dbname={$db};charset=utf8mb4",
-        $user,
-        $pass,
-        [
-          PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-          PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ]
-      );
+    private function __construct()
+    {
     }
 
-    return self::$conn;
-  }
+    public static function get(): PDO
+    {
+        if (self::$conn === null) {
+            if (esLocalhost()) {
+                $host = 'localhost';
+                $db = 'fygroup_antepuerto';
+                $user = 'adminfy';
+                $pass = 'seatrade1313';
+            } else {
+                $host = 'localhost';
+                $db = 'fygroupc_antepuerto';
+                $user = 'fygroupc_adminfy';
+                $pass = 'Seatrade1313_';
+            }
+
+            self::$conn = new PDO(
+                "mysql:host={$host};dbname={$db};charset=utf8mb4",
+                $user,
+                $pass,
+                [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,]
+            );
+        }
+
+        return self::$conn;
+    }
 }
